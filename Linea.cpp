bee6a5064b036a9ed9b133de79ae3a394e692d14
pizki3 <125227247+pizki3@users.noreply.github.com>
2024-05-07 17:46:04 -0500

    Update Linea.cpp

--- Linea.cpp
+++ Linea.cpp
@@ -16,12 +16,38 @@ Linea::Linea(int _Nest, string _NombreEs,string _Estaciones[100]) {
 
 
 void Linea::crear() {
-    
-}
+    int opcion;
+    cout <<"Despues de que estacion quiere crear la estacion:"<<endl;
+    cout<<"0: Primera estacion"<<endl;
+    for (int i=0; i<Nest;i++){
+        cout <<i+1<<": "<< Estaciones[i]<<endl;}
+    cin >> opcion;
+    for (int i = Nest; i>opcion; --i) {
+        Estaciones[i]=Estaciones[i - 1];}
+    cout<<"Ingrese nombre de la Estacion"<<endl;
+    string nombre;
+    cin>>nombre;
+    for (int i = 0; i <=Nest; ++i) {
+        if (Estaciones[i] == nombre) {
+            cout<<"Esta estacion ya existe"<<endl;
+            return;}}
+    Estaciones[opcion] =nombre;}
+
 
 void Linea::eliminar() {
-}
+    cout <<"Que estacion desea eliminar:"<<endl;
+    for (int i=0; i<Nest;i++){
+        cout <<i+1<<": "<< Estaciones[i]<<endl;}
+    int opcion;
+    cin >> opcion;
+    opcion=opcion-1;
+    for (char c : Estaciones[opcion]) {
+        if (c=='_'){
+            cout <<"Esta estacion es de transferencia no es posible eliminar"<<endl;
+            return;}}
+    for (int i = opcion; i<Nest- 1; ++i) {
+            Estaciones[i] = Estaciones[i + 1];}
+    Nest--;}
 
-int Linea::contarE() {
-    
-    return Nest;}
+void Linea::contarE() {
+    cout<<"Esta linea tiene:"<<Nest<<" estaciones"<<endl;}
